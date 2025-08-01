a8326e1f23de51fc503f2e500f0dfead888e00e3
vaibhav709a <aspandey02e@gmail.com>
2025-08-01 22:59:14 +0530

    Update data_feed.py

--- data_feed.py
+++ data_feed.py
@@ -1,17 +1,21 @@
 from twelvedata import TDClient
 import os
 
+# Put your TwelveData API key here if you're not using environment variables
 TD_API_KEY = os.getenv("TD_API_KEY", "806dd29a09244737ae6cd1a305061557)
 
 def get_live_candles(symbol="EUR/USD", interval="1min", outputsize=5):
     td = TDClient(apikey=TD_API_KEY)
+
     ts = td.time_series(
         symbol=symbol,
         interval=interval,
         outputsize=outputsize,
         timezone="UTC"
     )
+
     candles = ts.as_json()
+
     formatted = []
     for c in candles:
         formatted.append({
@@ -21,4 +25,5 @@ def get_live_candles(symbol="EUR/USD", interval="1min", outputsize=5):
             "low": float(c["low"]),
             "close": float(c["close"])
         })
+
     return formatted[::-1]
